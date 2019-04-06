3947d1cbdc64dccee5b124f8f0e5cab0f2e13bb7
DavidMR13840 <43644507+DavidMR13840@users.noreply.github.com>
2019-04-06 06:35:46 -0600

    Finalizar progra
    Se realizaron todos los metodos solicitados faltantes, se ejecuta la logica y la interfaz juntas dentro del controlador

--- ProyectoPOO1/src/Controller/GameController.java
+++ ProyectoPOO1/src/Controller/GameController.java
@@ -9,7 +9,17 @@ import GameLogic.*;
 import View.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+import javax.swing.JFileChooser;
 /**
  *
  * @author David
@@ -17,13 +27,199 @@ import java.util.ArrayList;
 public class GameController {
     MainFrame View;
     Game gameModel;
-
+    String fileName;
+    
     public GameController(MainFrame View, Game gameModel) {
         this.View = View;
         this.gameModel = gameModel;
-        
+        this.View.addSave_Listener(new saveListener());
+        this.View.addLoad_listener(new loadListener());
         this.View.addStart_newGameListener(new startListener());
+        this.View.addGameStart_listener(new gameStartListener());
+        this.View.addRollDice_listener(new rollDiceLisener());
+        this.View.disableRollDiceButton(false);
+        
     }
+
+    private  class loadListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Gson gson = new Gson();               
+            JFileChooser fileChooser = new JFileChooser(new File("."));
+            fileChooser.addChoosableFileFilter(new MyFilter());
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            fileChooser.showOpenDialog(null);
+            
+            fileName = fileChooser.getSelectedFile().getName();
+        try (Reader reader = new FileReader(fileName)) {
+
+			// Convert JSON to Java Object
+            gameModel = gson.fromJson(reader, Game.class);
+
+        } catch (IOException ex) {
+            View.displayError_message("Invalid file type");
+            ex.printStackTrace();
+        }
+            ArrayList<String> names = new ArrayList<>();
+            gameModel.getplayers().forEach((player) -> {
+                names.add(player.getName());
+            });
+            View.setPlayersinBoardGUI(names);
+            
+            View.setActive_playerLabel(gameModel.getPlayer().getName());
+            View.startNewGame();
+            if(gameModel.isStarted()){
+                View.disableRollDiceButton(true);
+                View.disableGamestartbtn();
+                 switch(gameModel.getplayers().size()){
+                       case 2:
+                           View.GUIboard.moverFicha(1, gameModel.getPlayer(1).getPosition(), gameModel.getPlayer(2).getPosition());
+                           break;
+                       case 3:
+                           View.GUIboard.moverFicha(1, gameModel.getPlayer(1).getPosition(), gameModel.getPlayer(2).getPosition(), gameModel.getPlayer(3).getPosition());
+                           break;
+                       case 4:
+                            View.GUIboard.moverFicha(1, gameModel.getPlayer(1).getPosition(), gameModel.getPlayer(2).getPosition(), gameModel.getPlayer(3).getPosition()
+                                ,gameModel.getPlayer(4).getPosition());
+                            break;
+                   }
+            }else{
+                View.disableRollDiceButton(false);
+            }
+            
+        
+        }
+
+    
+    }
+    
+    
+    private class saveListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Gson gson = new Gson();
+            //String jsonStringView = gson.toJson(View);
+            
+            if(!gameModel.isSaved()){
+                fileName = JOptionPane.showInputDialog(View, "Choose a name for the file") + ".json";
+                gameModel.save();
+            }
+            try {
+            String jsonStringModel = gson.toJson(gameModel);
+            FileWriter fileModel = new FileWriter(fileName);
+            fileModel.write(jsonStringModel);
+            fileModel.flush();
+        } catch (IOException ex) {
+            
+            //ex.printStackTrace();
+        }
+            catch (Exception e1){
+                View.displayError_message("Invalid file type");
+            }
+            
+        }
+        
+    }
+    private  class gameStartListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Object[] option1 = {"Roll Dice"};
+            ArrayList<Player> competitors = gameModel.getplayers();
+            while(!gameModel.isFirstPlayerSet()){
+                //competitors = gameModel.setPlayersTurn(competitors);
+                gameModel.setStart(true);
+                for(Player p : competitors){
+                    JPanel panel = new JPanel();
+                    panel.add(new JLabel(p.getName()));
+                    panel.add(new JLabel("Last roll: " + p.getFirstGO()));
+                    int result = JOptionPane.showOptionDialog(View, panel, "Roll highest number to start first", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE, null, option1, null);
+                    if(result == 0){
+                       int dicevalue = gameModel.playerRollDice(p);
+                       View.displayDice(dicevalue);
+                    }
+                }
+                competitors = gameModel.setPlayersTurn(competitors);
+                
+            }
+            View.disableRollDiceButton(true);
+            gameModel.SortPlayers(gameModel.getPlayer());
+            ArrayList<String> names = new ArrayList<>();
+            gameModel.getplayers().forEach((player) -> {
+                names.add(player.getName());
+            });
+            View.setPlayersinBoardGUI(names);
+            View.disableGamestartbtn();
+            gameModel.resetPlayers_firstRoll();
+            View.setActive_playerLabel(gameModel.getPlayer().getName());
+
+        }
+        
+    }
+
+    private  class rollDiceLisener implements ActionListener{
+        int diceValue = 0;
+        @Override
+        public void actionPerformed(ActionEvent e) {
+                if(!gameModel.hasWon()){
+                    View.setActive_playerLabel(gameModel.getPlayer().getName());
+                    if(!gameModel.playerStart()){
+                        int roll =gameModel.playerRollDice_f();
+                        if(roll == 5){    
+                           
+                            View.displayDice(-1);
+                        }else{   
+                            gameModel.getNextPlayer();
+                            View.displayDice(0);
+                        }
+                        
+                        
+                    }else{
+                   diceValue = gameModel.rollDice();
+                   View.displayDice(diceValue);
+                    
+                   
+                  
+                   int pos_bMoving = gameModel.getPlayer().getPosition();
+                    View.displayActive_pos(pos_bMoving);
+                   try{
+                      View.displayActive_pos(gameModel.getPlayer().getPosition());
+                      Player moving_player = gameModel.getPlayer();
+                       gameModel.movePlayer(diceValue);
+                       View.displayActive_pos(moving_player.getPosition());
+                   }catch(Exception evtMove){
+                       View.displayDice(0);
+                   }
+                   switch(gameModel.getplayers().size()){
+                       case 2:
+                           View.GUIboard.moverFicha(pos_bMoving, gameModel.getPlayer(1).getPosition(), gameModel.getPlayer(2).getPosition());
+                           break;
+                       case 3:
+                           View.GUIboard.moverFicha(pos_bMoving, gameModel.getPlayer(1).getPosition(), gameModel.getPlayer(2).getPosition(), gameModel.getPlayer(3).getPosition());
+                           break;
+                       case 4:
+                            View.GUIboard.moverFicha(pos_bMoving, gameModel.getPlayer(1).getPosition(), gameModel.getPlayer(2).getPosition(), gameModel.getPlayer(3).getPosition()
+                                ,gameModel.getPlayer(4).getPosition());
+                            break;
+                   }
+                  
+                }
+                       
+                }else{
+                    JOptionPane.showMessageDialog(View, "Player " + gameModel.getWinner().getName()+ " has won!");
+                }
+                
+                
+                
+            
+        }
+    }
+    
+    
+    
+    
     
     public class startListener implements ActionListener{
 
@@ -31,15 +227,30 @@ public class GameController {
         public void actionPerformed(ActionEvent e) {
             try{
                 ArrayList<String> playerNames = View.getPlayersNames();
-                gameModel.addPlayers(View.getPlayersNames());
+                gameModel.addPlayers(playerNames);
+                
                 View.startNewGame();
+                View.setExtendedState(View.getExtendedState() | View.MAXIMIZED_BOTH );
             }catch(IllegalArgumentException exception){
                 View.displayError_message(exception.getMessage());
             }
             
+            
         }
             
         }
+    
+    class MyFilter extends javax.swing.filechooser.FileFilter {
+  @Override
+  public boolean accept(File file) {
+    String filename = file.getName();
+    return filename.endsWith(".json");
+  }
+        @Override
+        public String getDescription() {
+            return "*.json";
+        }
         
     
+}
 }
--- ProyectoPOO1/src/GameLogic/Game.java
+++ ProyectoPOO1/src/GameLogic/Game.java
@@ -7,7 +7,6 @@ package GameLogic;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Random;
 
@@ -18,10 +17,41 @@ import java.util.Random;
 public class Game {
     private int dice;
     boolean finished;
-    ArrayList<Player> players = new ArrayList<>();
+    private ArrayList<Player> players = new ArrayList<>();
+    private Player active_player;
+    boolean obtainedFirstPlayer = false;
+    boolean started = false;
+    boolean saved = false;
+    private  Map<Integer, Integer> snakeMap = new HashMap<>();
+    private  Map<Integer, Integer> ladderMap = new HashMap<>();
     
-    private static Map<Integer, Integer> snakeMap = new HashMap<Integer,Integer>();
-    private static Map<Integer, Integer> ladderMap = new HashMap<Integer,Integer>();
+public Game(){
+    snakeMap.put(37, 13);
+    snakeMap.put(65, 50);
+    snakeMap.put(71, 34);
+    snakeMap.put(80, 2);
+    snakeMap.put(91, 66);
+    snakeMap.put(99, 15);
+    snakeMap.put(87, 51);
+    
+    ladderMap.put(4, 79);
+    ladderMap.put(10, 28);
+    ladderMap.put(20, 98);
+    ladderMap.put(25, 67);
+    ladderMap.put(47, 76);
+    ladderMap.put(77, 95);
+    ladderMap.put(82, 93);
+    
+}
+    public void save(){
+        saved = true;
+    }
+    
+    public boolean isSaved(){
+        return saved;
+    }
+    
+
     
     
     private static int getRandomNumberInRange(int min, int max) {
@@ -39,38 +69,39 @@ public class Game {
         return dice;
     }
     
-    private boolean playerStart(Player p){
-        if (p.getFirstGO() == 5) {
-            return true;
-        }else{
-            p.setFirstGO(rollDice());
-            return p.getFirstGO() == 5;
-        }
+    public boolean playerStart(){
+        return active_player.getFirstGO() == 5;
     }
     
-    public Player obtainFirstPlayer(ArrayList<Player> pList){
-        ArrayList<Player> rePlayers = new ArrayList<>();
-        for(Player player : pList){
-            player.setFirstGO(rollDice());
+    public Player getPlayer(){
+        return active_player;
+    }
+    
+    public Player getNextPlayer(){
+        if(players.indexOf(active_player) == players.size()-1){
+            active_player = players.get(0);
+        }else{
+            active_player = players.get(players.indexOf(active_player)+1);
         }
-        Iterator i = pList.iterator();
-        Player p = (Player)i.next();
-        rePlayers.add(p);
-        while(i.hasNext()){
-            Player nextPlayer =(Player)i.next();
-            if(p.getFirstGO() < nextPlayer.getFirstGO()){
-                rePlayers.clear();
-                p = nextPlayer;
-                rePlayers.add(p);
-            }else if(p.getFirstGO() == nextPlayer.getFirstGO()){
-                rePlayers.add(p);
-            }
+        return active_player;
+    }
+    
+    public int playerRollDice_f(){
+        int dice_value = rollDice();
+        active_player.setFirstGO(dice_value);
+        return dice_value;
+    }
+    
+    public int playerRollDice(Player player){
+        int dice_value = rollDice();
+        player.setFirstGO(dice_value);
+        return dice_value;
+    }
+    
+    public void resetPlayers_firstRoll(){
+        for(Player p : players){
+            p.setFirstGO(0);
         }
-        p = obtainFirstPlayer(rePlayers);
-        for(Player player : pList){
-            player.setFirstGO(0);
-        }
-        return p;
     }
     
     public void SortPlayers(Player p){
@@ -87,16 +118,19 @@ public class Game {
             aux.add(players.get(pos));
             pos++;
         }
-        
+             
         players = aux;
+        
+        for(Player player : players){
+            player.setidOrder(players.indexOf(player)+1);
+        }
     }
     
     
-    public int movePlayer(Player player) throws CantMoveException{
-        int actualPos = player.getPosition();
-        if(playerStart(player)){
-        dice = rollDice();
-        actualPos += dice;
+    public int movePlayer(int rolled) throws CantMoveException{
+        int actualPos = active_player.getPosition();
+        if(playerStart()){
+        actualPos += rolled;
         
         if(actualPos > 100){
             int i = actualPos - 100;
@@ -110,24 +144,96 @@ public class Game {
         if(null != ladderMap.get(actualPos)){
             actualPos = ladderMap.get(actualPos);
         }
-        player.setPosition(actualPos);
-        return dice;
+        active_player.setPosition(actualPos);
+        if(rolled != 6){
+            getNextPlayer();
+        }
+        return actualPos;
         }else{
+            getNextPlayer();
             throw new  CantMoveException("Player cannot move!");
         }
     }
     
-    public boolean hasWon(Player player){
-        return player.getPosition() == 100;
+    public boolean hasWon(){
+        boolean won = false;
+        for(Player p : players){
+            if(p.getPosition() == 100){
+                won = true;
+                break;
+            }
+        }
+        return won;
     }
     
     public void addPlayers(ArrayList<String> names){
         for(String player_nameString : names){
             addPlayer(1, player_nameString, 1);
-        }    
+        }
+        active_player = players.get(0);
+    }
+    
+    public ArrayList<Player> setPlayersTurn(ArrayList<Player> same_rollPlayers){      
+        ArrayList<Player> result = new ArrayList<>();
+        Player highestRollPlayer = same_rollPlayers.get(0);
+        int i = 1;
+        if(started){
+            result.add(highestRollPlayer);
+            while( i < same_rollPlayers.size()){
+                if(same_rollPlayers.get(i).getFirstGO() > highestRollPlayer.getFirstGO()){
+                    highestRollPlayer = same_rollPlayers.get(i);
+                    result.clear();
+                    result.add(highestRollPlayer);
+                }else if(same_rollPlayers.get(i).getFirstGO() == highestRollPlayer.getFirstGO()){
+                    result.add(same_rollPlayers.get(i));
+                }
+                i++;
+            }
+            if(result.size()== 1){
+            obtainedFirstPlayer = true;
+            active_player = result.get(0);
+            }   
+        }else{
+            started = true;
+            return same_rollPlayers;
+        }
+   
+        return result;
+    }
+    
+    public boolean isStarted(){
+        return started;
+    }
+    public void setStart(boolean started){
+        this.started = started;
+    }
+    public boolean isFirstPlayerSet(){
+        return obtainedFirstPlayer;
+    }
+    public  ArrayList<Player> getplayers(){
+        
+        return players;
     }
     
     public void addPlayer(int pos, String name, int lvl){
         players.add(new Player(pos, name, lvl));
+    }
+    
+    public Player getWinner(){
+        while(active_player.getPosition() != 100){
+            getNextPlayer();
+        }
+        return active_player;
+    }
+    
+    public Player getPlayer(int player_number){
+        Player player = active_player;
+        for(Player p : players){
+            if(p.getidOrder() == player_number){
+                player = p;
+                break;
+            }
+        }
+        return player;
     }
 }
